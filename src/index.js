38d82be44007245694af01e461e2a54051a7c88a
xydotguy <Yang.Xu@lexisnexis.com>
2016-11-22 22:55:36 -0500

    Add new library

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var uniqueRandomArray = require('unique-random-array');
+var starWarNames = require('./starwars-names.json');
+
+module.exports = {
+    all:starWarNames,
+    random:uniqueRandomArray(starWarNames)
+
+};
